0f508532a1cf243ffb3da64b8bc0869e6f214a9c
Lauro-Santos <32677114+Lauro-Santos@users.noreply.github.com>
2023-02-27 00:43:41 -0300

    implementação: Highlight e alterar cor

--- js/highlight.js
+++ js/highlight.js
@@ -1,14 +1,16 @@
 const linguagem = document.querySelector('[data-linguagem]')
-const areaDoCodigo = document.querySelector('[data-editorDeCodigo]')
+const areaDoCodigo = document.querySelector('[data-areaDoCodigo]')
 const btnHighlight = document.querySelector('[data-aplicarHighlight]')
 
 function aplicarHighlight() {
     const codigo = areaDoCodigo.innerText
     areaDoCodigo.innerHTML = `
-    <code class="editor hljs ${linguagem.value}" contenteditable="true" aria-label="Editor de código">function test(){}</code>
+    <code class="editor hljs ${linguagem}" aria-label="Editor de código" autofocus contenteditable="true" data-editor></code>
     `
-    areaDoCodigo.querySelector('.editor').textContent = codigo
-    hljs.highlightElement(areaDoCodigo.querySelector('.editor'))
+    areaDoCodigo.querySelector('code').textContent = codigo
+    console.log(areaDoCodigo)
+    
+    hljs.highlightElement(areaDoCodigo.querySelector('code'))
 }
 
 btnHighlight.addEventListener('click', () => {
--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const tagCor = document.querySelector('[data-seletorDeCor]')
 const fundo = document.querySelector('[data-fondoEditor]')
 
+const editor = document.querySelector('[data-editor]')
+
 function alterarCorDeFundo() {
     const cor = tagCor.value
     fundo.style.background = cor
@@ -9,4 +11,3 @@ function alterarCorDeFundo() {
 tagCor.addEventListener('focusout', () => {
     alterarCorDeFundo()
 })
-
